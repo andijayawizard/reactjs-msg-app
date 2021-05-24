6593ae61469f9296446394b37403cd370edbdffd
andijayawizard <33024329+andijayawizard@users.noreply.github.com>
2021-05-24 11:32:51 +0700

    2405

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import "./App.scss";
+import { GlobalStyles } from "./components/globalStyles";
+import { darkTheme, lightTheme } from "./components/Themes";
+import Toggle from "./components/Toggler";
+import { useDarkMode } from "./components/useDarkMode";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
-import Products from './pages/Products'
+import Products from "./pages/Products";
 
 function App() {
+  // const [theme, setTheme] = useState("light");
+  // const themeToggler = () => {
+  //   theme === "light" ? setTheme("dark") : setTheme("light");
+  // };
+  const [theme, themeToggler, mountedComponent] = useDarkMode();
+  const themeMode = theme === "light" ? lightTheme : darkTheme;
+  if(!mountedComponent) return <div/>
   return (
-    <Router>
-      <Route exact path="/" component={Home}></Route>
-      <Route path="/about" component={About}></Route>
-      <Route path="/contact" component={Contact}></Route>
-      <Route path="/products" component={Products}></Route>
-    </Router>
+    <ThemeProvider theme={themeMode}>
+      <>
+        <GlobalStyles />
+        <Toggle theme={theme} toggleTheme={themeToggler} />
+        {/* <Button onClick={themeToggler}>Switch Theme</Button> */}
+        <Router>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/about" component={About}></Route>
+          <Route path="/contact" component={Contact}></Route>
+          <Route path="/products" component={Products}></Route>
+        </Router>
+      </>
+    </ThemeProvider>
   );
 }
 export default App;
